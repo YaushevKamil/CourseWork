76ce40c6fbbcc50bbac69a3ebe2c1ab12477dac7
YaushevKamil <kyaush@mail.ru>
2019-12-29 02:31:08 +0300

    skybox.hpp added

---
+++ src/include/skybox.hpp
@@ -0,0 +1,19 @@
+#ifndef SKYBOX_HPP
+#define SKYBOX_HPP
+
+#include "shader.hpp"
+
+class Skybox {
+public:
+    Skybox(GLuint texture, Shader *shader);
+    ~Skybox();
+    void draw();
+private:
+    GLuint  VAO;
+    GLuint  VBO;
+    GLuint  EBO;
+    GLuint  texture;
+    Shader *shader;
+};
+
+#endif // SKYBOX_HPP
