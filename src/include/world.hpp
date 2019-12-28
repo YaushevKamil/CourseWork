3e7fe0a8264c6c5ce0da3a06977c7fda8e71455e
YaushevKamil <kyaush@mail.ru>
2019-12-29 02:34:11 +0300

    world.hpp added

---
+++ src/include/world.hpp
@@ -0,0 +1,65 @@
+#ifndef WORLD_HPP
+#define WORLD_HPP
+
+#include <GL/glew.h>
+#include <cmath>
+#include <filesystem>
+#include <random>
+#include <vector>
+
+#include "json_config.hpp"
+//#include "table.hpp"
+#include "texture.hpp"
+
+//#include "bot.hpp"
+//#include "obstacle.hpp"
+#include "skybox.hpp"
+
+class World {
+public:
+    World(JSONConfig* config,
+          Shader *terrain_shader,
+          Shader *bot_shader,
+          Shader *plane_shader,
+          Shader *skybox_shader,
+          GLfloat *accumulation);
+    ~World();
+    void draw();
+private:
+    GLfloat *accumulation;
+
+//    Table* table;
+
+    Skybox* skybox;
+//    Obstacle* plane;
+//    Obstacle* grass;
+//    std::vector<Bot*> bots;
+//    std::vector<Obstacle*> obstacles;
+
+//    void loadTable(JSONConfig* config);
+    void loadSkybox(JSONConfig* config, Shader *shader);
+//    void genereateLife(JSONConfig* config,
+//                       Shader* plane_shader,
+//                       Shader* terrain_shader,
+//                       Shader* bot_shader);
+//
+//    void fillPlane(JSONConfig* config, Shader* shader);
+//    void fillGrass(JSONConfig* config, Shader* shader);
+//    void fillObstacle(JSONConfig* config, Shader* shader);
+//    void searchModels(std::vector<std::string> *models, std::string path);
+//    void fillBot(JSONConfig* config, Shader* shader);
+
+//    void makeMove();
+//    void addBot(GLuint colony, GLuint i, GLuint j);
+//    void fightBots(Organism* alpha, Organism* beta);
+//    void pairBots(Organism* alpha, Organism* beta);
+//    bool checkNeighbor(Organism *bot);
+//    void checkGrass(Organism *bot);
+//    void generateGrass();
+//    void toStep();
+//    void WarOrLive();
+//    void moveBot(Cell* from, Cell* to, GLuint i, GLuint j);
+//    void feed(Organism* bot, Cell* curr);
+};
+
+#endif // WORLD_HPP
